51b8bbb1aae946a0e79358e07bf3397d09da56cb
Ryan Peters <106757186+ryanpeters-MSFT@users.noreply.github.com>
2024-05-15 17:50:05 +0000

    input validation

--- static/js/app.js
+++ static/js/app.js
@@ -103,4 +103,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
         localStorage.setItem(HIGHLIGHTEDITEM, closestListItem.id);
     };
+
+    nameInput.addEventListener("keyup", function() {
+        const addButton = document.querySelector("button[id='addButton']");
+        addButton.disabled = this.value.trim() === "";
+    });
+
+    recognition.onresult = function(event) {
+        const transcript = event.results[0][0].transcript;
+        const recognizedText = transcript.endsWith('.') ? transcript.slice(0, -1) : transcript;
+        nameInput.value = recognizedText;
+    
+        const addButton = document.querySelector("button[id='addButton']");
+        addButton.disabled = false;
+    };
 });
+
