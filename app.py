5aded6aeddc99a0f52ee40b6078e493cb7c7b697
Ryan Peters <106757186+ryanpeters-MSFT@users.noreply.github.com>
2024-05-15 16:18:37 +0000

    caching AI results

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-###############################################################################
-## Sprint 3: Database Integration
-## Feature 2: Persist To-Do List
-## User Story 2: Load To-Do List
-###############################################################################
 import os
 from flask import Flask, render_template, request, redirect, url_for, g
 from database import db, Todo
+from recommendation_engine import RecommendationEngine
+
+import json
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))   # Get the directory of the this file
@@ -47,6 +45,34 @@ def remove_todo(id):
     db.session.delete(Todo.query.filter_by(id=id).first())
     db.session.commit()
     return redirect(url_for('index'))
+
+# Show AI recommendations
+@app.route('/recommend/<int:id>', methods=['GET'])
+async def recommend(id):
+    recommendation_engine = RecommendationEngine()
+    g.todo = db.session.query(Todo).filter_by(id=id).first()
+
+    if g.todo:
+        try:
+            #attempt to load any saved recommendation from the DB
+            if g.todo.recommendations_json is not None:
+                g.todo.recommendations = json.loads(g.todo.recommendations_json)
+                return render_template('index.html')
+        except ValueError as e:
+            print("Error:", e)
+
+    g.todo.recommendations = await recommendation_engine.get_recommendations(g.todo.name)
+        
+    # Save the recommendations to the database
+    try:
+        g.todo.recommendations_json = json.dumps(g.todo.recommendations)
+        db.session.add(g.todo)
+        db.session.commit()
+    except Exception as e:
+        print(f"Error adding and committing todo: {e}")
+        return
+
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- database.py
+++ database.py
@@ -10,6 +10,8 @@ db = SQLAlchemy(model_class=Base)
 class Todo(db.Model):
     id = db.Column(Integer, primary_key=True)
     name = db.Column(String(100), nullable=False)
+    recommendations = []
+    recommendations_json = db.Column(db.JSON)   
 
     def __str__(self):
         return self.name
